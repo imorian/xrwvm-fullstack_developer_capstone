d9e244deee804475104cd240345ae15f60f6821c
imorian <silvian,cedru@gmail.com>
2024-10-10 03:13:33 -0400

    module 3.2

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,6 +14,8 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
+from .models import CarMake, CarModel
+
 
 
 # Get an instance of a logger
@@ -45,13 +47,7 @@ def logout_request(request):
     data = {"userName": username}
     return JsonResponse(data)
 
-# Create a `registration` view to handle sign up request
-from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-import json
-import logging
+
 
 # Get an instance of a logger
 
@@ -103,6 +99,17 @@ def registration(request):
     data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
